34814c9b9e6c65448473b8c316fe6ab0963dff90
Sabahat Hussain <miansabahat10@gmail.com>
2022-07-29 18:17:40 +0500

    Google Ads added

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -2,6 +2,7 @@ import 'package:cached_map/cached_map.dart';
 import 'package:email_password_login/screens/auth_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -11,27 +12,21 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     navigate();
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Container(
-          width: 250,
-          height: 250,
-          child: Image.asset("assets/logo.png",)
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Container(
+            width: 250,
+            height: 250,
+            child: Image.asset("assets/logo.png",)
+          ),
         ),
       ),
     );
   }
 
   navigate() async {
-    String storeUrl;
-    try {
-      if (await canLaunch(storeUrl)) {
-        await launch(storeUrl, forceSafariVC: false);
-      }
-    } on PlatformException {
-      Fluttertoast.showToast(msg: "There is some problem with your App, Kindly update manually from $storeUrl");
-    }
     await Future.delayed(Duration(seconds: 3));
     Mapped.loadFileDirectly(cachedFileName: "slider").then((file) {
       Get.toNamed(
